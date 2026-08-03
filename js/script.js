7caec18b1c78b220803b2665a3b8ddad14dec3dc
Fabiane Müller <fabiane-mueller@mail.de>
2026-08-03 20:24:29 +0200

    Details for Dialog

--- js/script.js
+++ js/script.js
@@ -1,76 +1,110 @@
 const pokemonBaseUrl = "https://pokeapi.co/api/v2/pokemon";
 const pokemonLimitPath = "?limit=";
-const pokemonLimit = 12;
+let pokemonLimit = 12;
 const pokemonOffsetPath = "&offset=";
-const pokemonOffset = 0;
+let pokemonOffset = 0;
 const pokemonImagePath =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
-const pokemonUrl =   pokemonBaseUrl +  pokemonLimitPath +  pokemonLimit +  pokemonOffsetPath +  pokemonOffset;
+const pokemonUrl =
+  pokemonBaseUrl +
+  pokemonLimitPath +
+  pokemonLimit +
+  pokemonOffsetPath +
+  pokemonOffset;
 
 let pokemonList = [];
 
-
-
-async function init(){
+async function init() {
   await catchBase();
 }
 
-
-async function catchBase(){
+async function catchBase() {
   let response = await fetch(pokemonUrl);
   let responseToJson = await response.json();
   let baseResults = responseToJson.results;
-  console.log(baseResults);
   catchDetails(baseResults);
-  
 }
 
-async function catchDetails(baseResults){
+async function catchDetails(baseResults) {
   for (let i = 0; i < baseResults.length; i++) {
-     const pokemonDetailUrl = baseResults[i].url;
-     let response = await fetch(pokemonDetailUrl);
-     let responseToJson = await response.json();
-     console.log(responseToJson);
-     let pokemon = {};
-     pokemon.name = [];
-     pokemon.name = responseToJson.name;
-     pokemon.id = responseToJson.id;
-     pokemon.types = [];
-     pokemon.types = responseToJson.types;
-pokemon.types = [];
-     for (let i = 0; i < responseToJson.types.length; i++) {
-      pokemon.types.push(responseToJson.types[i].type.name);
-     }
-catchSpecies(responseToJson, pokemon);
-     pokemon.height = responseToJson.height;
-     pokemon.weight = responseToJson.weight;
-     pokemon.abilities = [];
-     for (let i = 0; i < responseToJson.abilities.length; i++) {
-      pokemon.abilities.push(responseToJson.abilities[i].ability.name);
-     }
-     pokemon.stats = [];
-     for (let i = 0; i < responseToJson.stats.length; i++) {
-      pokemon.stats.push(responseToJson.stats[i].base_stat);
-      pokemon.stats.push(responseToJson.stats[i].stat.name);
-     }
-pokemonList.push(pokemon);
-
+    const pokemonDetailUrl = baseResults[i].url;
+    let response = await fetch(pokemonDetailUrl);
+    let responseToJson = await response.json();
+    let pokemon = {};
+    pokemon.name = [];
+    pokemon.name = responseToJson.name;
+    pokemon.id = responseToJson.id;
+    pokemon.height = responseToJson.height;
+    pokemon.weight = responseToJson.weight;
+    catchTypes(responseToJson, i, pokemon);
+    catchStats(responseToJson, i, pokemon);
+    catchSpecies(responseToJson, pokemon);
+    catchAbilities(responseToJson, i, pokemon)
+    pokemonList.push(pokemon);
   }
-renderPokemon()
-    console.log(pokemonList);
+  renderPokemon();
 }
 
-async function catchSpecies(responseToJson,pokemon){
+function catchAbilities(responseToJson, i, pokemon){
+   pokemon.abilities = [];
+    for (let i = 0; i < responseToJson.abilities.length; i++) {
+      pokemon.abilities.push(responseToJson.abilities[i].ability.name);
+    }
+}
+
+function catchTypes(responseToJson, i, pokemon){
+   pokemon.types = [];
+    pokemon.types = responseToJson.types;
+    pokemon.types = [];
+    for (let i = 0; i < responseToJson.types.length; i++) {
+      pokemon.types.push(responseToJson.types[i].type.name);
+    }
+}
+
+function catchStats(responseToJson, i, pokemon) {
+    pokemon.stats = [];
+
+    for (let i = 0; i < responseToJson.stats.length; i++) {
+        pokemon.stats.push({
+            name: responseToJson.stats[i].stat.name,
+            value: responseToJson.stats[i].base_stat
+        });
+    }
+}
+
+async function catchSpecies(responseToJson, pokemon) {
   let speciesUrl = responseToJson.species.url;
   let response = await fetch(speciesUrl);
   let responseToJsonSpecies = await response.json();
-console.log(responseToJsonSpecies);
-pokemon.eggGroup = [];
-     for (let i = 0; i < responseToJsonSpecies.egg_groups.length; i++) {
-      pokemon.eggGroup.push(responseToJsonSpecies.egg_groups[i].name);
-     }
+  pokemon.eggGroup = [];
+  for (let i = 0; i < responseToJsonSpecies.egg_groups.length; i++) {
+    pokemon.eggGroup.push(responseToJsonSpecies.egg_groups[i].name);
+  }
 }
 
+
+function backgrounds(pokemon, i) {
+  const background = document.getElementById(`card${i}`);
+ 
+  if (pokemon.types.includes("fire")) {
+    background.classList.add("fire-card");
+  }
+  if (pokemon.types.includes("water")) {
+    background.classList.add("aqua-card");
+  }
+  if (pokemon.types.includes("electric")) {
+    background.classList.add("electric-card");
+  }
+  if (pokemon.types.includes("grass")) {
+    background.classList.add("grass-card");
+  }
+  if (pokemon.types.includes("bug")) {
+    background.classList.add("grass-card");
+  }
+  if (pokemon.types.includes("stone")) {
+    background.classList.add("stone-card");
+  }
+}
 
 
 function renderPokemon() {
@@ -78,33 +112,78 @@ function renderPokemon() {
   for (let i = 0; i < pokemonList.length; i++) {
     const pokemonIndex = i + 1;
     const pokemonImage = pokemonImagePath + pokemonIndex + ".png";
-     cardRef.innerHTML += getcardHtml(pokemonList,i,pokemonIndex);
-     const pokemon = pokemonList[i];
-      backgrounds(pokemon,i);
-     }
+    cardRef.innerHTML += getcardHtml(pokemonList, i, pokemonIndex);
+    const pokemon = pokemonList[i];
+    backgrounds(pokemon, i);
+  }
 }
 
-function backgrounds(pokemon, i){
-const background = document.getElementById(`card${i}`);
-if(pokemon.types.includes("fire")){
-  background.classList.add("fire-card")
-}
-if(pokemon.types.includes("water")){
-  background.classList.add("aqua-card")
-}
-if(pokemon.types.includes("electric")){
-  background.classList.add("electric-card")
-}
-if(pokemon.types.includes("grass")){
-  background.classList.add("grass-card")
-}
-if(pokemon.types.includes("bug")){
-  background.classList.add("grass-card")
-}
-if(pokemon.types.includes("stone")){
-  background.classList.add("stone-card")
+const dialogRef = document.getElementById("dialog");
+
+function openDialog(i) {
+  dialogRef.showModal();
+  renderDialog(i);
 }
 
+function renderDialog(i) {
+  console.log(pokemonList);
+  backgroundImage(i);
+  
+  dialog.innerHTML = getDialogHtml(i);
+  console.log(pokemonList);
+}
+
+function closeDialog() {
+  dialogRef.close();
+}
+
+
+
+function backgroundImage(i){
+  const cls = ["grass-dialog", "fire-dialog", "aqua-dialog", "stone-dialog", "electric-dialog"]
+ if (pokemonList[i].types.includes("fire")) {
+    dialogRef.classList.remove(...cls);
+    dialogRef.classList.add("fire-dialog");
+  }
+  if (pokemonList[i].types.includes("grass")) {
+    dialogRef.classList.remove(...cls);
+    dialogRef.classList.add("grass-dialog");
+  }
+   if (pokemonList[i].types.includes("bug")) {
+    dialogRef.classList.remove(...cls);
+    dialogRef.classList.add("grass-dialog");
+  }
+  if (pokemonList[i].types.includes("water")) {
+    dialogRef.classList.remove(...cls);
+    dialogRef.classList.add("aqua-dialog");
+  }
+  if (pokemonList[i].types.includes("stone")) {
+    dialogRef.classList.remove(...cls);
+    dialogRef.classList.add("stone-dialog");
+  }
+   if (pokemonList[i].types.includes("electric")) {
+    dialogRef.classList.remove(...cls);
+    dialogRef.classList.add("electric-dialog");
+  }
+}
+
+function nextPokemon(i){
+ 
+
+  if(i +1 < pokemonList.length){
+     i++;
+  }
+  else{
+    i =0;
+  }
+
+renderDialog(i);
+}
+
+function loadMore(){
+  pokemonLimit += 12;
+  pokemonOffset += 12;
+  renderPokemon();
 }
 
 
--- js/templates.js
+++ js/templates.js
@@ -1,7 +1,7 @@
 function getcardHtml(pokemonList,i,pokemonIndex){
 
     return `
-     <button id="card${i}" class="card-box" aria-haspopup="dialog" type="button" tabindex="0">
+     <button id="card${i}" class="card-box" aria-haspopup="dialog" type="button" tabindex="0" onclick="openDialog(${i})">
                     <div class="card-header">
                         <h3 id="cardPokemonName${i}" class="card-title">${pokemonList[i].name}</h3>
                         <ul class="type-box" id="type">
@@ -13,12 +13,113 @@ function getcardHtml(pokemonList,i,pokemonIndex){
                             <p>${pokemonList[i].id}</p>
                         </div>
                         <div class="card-image" id="pokemonCardImage">
-                            <img src="${pokemonImagePath + pokemonIndex + ".png"}" alt="Pokemon:">
+                            <img src="${pokemonImagePath + pokemonIndex + ".png"}" alt="Pokemon:${pokemonList[i].name}">
                         </div>
                     </div>
                 </button>
     `
 }
+
+function getDialogHtml(i){
+    const pokemonIndex = i + 1;
+    return /*html*/`
+        <div class="dialog-head">
+                    <button onclick="closeDialog()" class="back-btn" id="backToAllButton"><img src="./assets/icons/back-arrow.png"
+                            alt="Back Arrow"></button>
+                </div>
+                <div class="dialog-header">
+                    <h3 id="dialogPokemonName" class="dialog-title">${pokemonList[i].name}</h3>
+                    <ul class="type-box" id="type">
+                            ${getTypesHtml(pokemonList[i].types)}
+                    </ul>
+                </div>
+                <div class="dialog-hero">
+                    <button class="sound-btn" id="pokemonCry"><img src="./assets/icons/grass-cry-button.png"
+                            alt="Sound Icon"></button>
+                            <img src="${pokemonImagePath + pokemonIndex + ".png"}" alt="Pokemon:${pokemonList[i].name}"  id="pokemonDialogImage" class="dialog-image">
+                </div>
+                <div class="dialog-content">
+                    <div class="dialog-navigation">
+                        <button class="" onclick="openDialogTab('about')">About</button>
+                        <button class="" onclick="openDialogTab('baseStats')">Base stats</button>
+                        <button class="" onclick="openDialogTab('evolution')">Evolution</button>
+                    </div>
+                    <div id="about" class="dialogTab">
+                        <div class="basics-box">
+                            <div class="basic-box">
+                                <img src="./assets/icons/species.png" alt="Icon Species">
+                                <div class="basic-content">
+                                    <p id="species">Seed</p>
+                                    <p class="basic-desc">Species </p>
+                                </div>
+                            </div>
+                            <div class="basic-box">
+                                <img src="./assets/icons/messen.png" alt="Icon Species">
+                                <div class="basic-content">
+                                    <p id="species">${pokemonList[i].height}"</p>
+                                    <p class="basic-desc">Height</p>
+                                </div>
+                            </div>
+                            <div class="basic-box">
+                                <img src="./assets/icons/gewicht.png" alt="Icon Species">
+                                <div class="basic-content">
+                                    <p id="species">${pokemonList[i].weight} lbs</p>
+                                    <p class="basic-desc">Weight</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="star-box">
+                            <div class="star-box-content">
+                                <img src="./assets/icons/stars.png" alt="Stars Icon">
+                                <ul id="abilities" class="abilities-list">
+                                    ${getAbilitiesHtml(pokemonList[i].abilities)}
+                                </ul>
+                                
+                            </div>
+                            <p>Abilities</p>
+                        </div>
+                        
+                    </div>
+
+                    <div id="baseStats" class="dialogTab active" style="display:none">
+                        <div class="stats-box">
+                            ${getStatsHtml(pokemonList[i].stats)}
+                        </div>
+                    </div>
+
+                    <div id="evolution" class="dialogTab" style="display:none">
+                        <div class="evolution-box">
+                            <div class="evolution-item">
+                                <div class="evolution-content">
+                                    <h4>Bulbasaur</h4>
+                                    <p><span>Nr.</span> 0001</p>
+                                </div>
+                                <img src="./assets/img/2.png" alt="Bulbasaur">
+                            </div>
+
+                            <div class="next-evolution">
+                                <div class="evolution-content">
+                                    <p class="level-box">@ Level 16</p>
+                                </div>
+                                <div class="evolution-arrow"><img src="./assets/icons/arrow-down.png"
+                                        alt="Arrow Down Icon"></div>
+                            </div>
+                        </div>
+                    </div>
+                    
+
+                </div>
+
+                <div class="dialog-bottom-nav" id="dialogBottomNav">
+                    <ul>
+                        <li><button><img src="./assets/icons/prev-btn.png" alt="Previous Icon"></button></li>
+                        <li><button onclick="nextPokemon(i)"><img src="./assets/icons/next-btn.png" alt="Next Icon"></button></li>
+                    </ul>
+                </div>
+    `
+}
+
+
 
 
 function getTypesHtml(types) {
@@ -28,6 +129,35 @@ function getTypesHtml(types) {
         html += /*html*/`
             <li class="type">${types[i]}</li>
         `;
+    }
+    return html;
+}
+
+
+function getAbilitiesHtml(abilities){
+    let html = "";
+    for (let i = 0; i < abilities.length; i++) {
+        html += /*html*/`
+            <li>${abilities[i]}</li>
+        `;
+    }
+    return html;
+}
+
+
+function getStatsHtml(stats){
+    let html = "";
+    for (let i = 0; i < stats.length; i++) {
+        html += /*html*/`
+        <div class="stats-item">
+            <label for="file">${stats[i].name}</label>
+            <span class="stat-value">${stats[i].value}</span>
+            <div class="stat-bar">
+                <div class="stat-fill" style="width:${stats[i].value}%"></div>
+            </div>
+        </div>
+        `
+        
     }
     return html;
 }
